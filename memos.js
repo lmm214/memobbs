754513e787b366f690211c2ce796787b1a9d52df
lmm214 <fivemu@gmail.ocm>
2024-02-01 16:14:13 +0800

    fix

--- memos.js
+++ memos.js
@@ -341,13 +341,15 @@ document.addEventListener("DOMContentLoaded", async () => {
 async function getMemoListData(url) {
   const response = await fetch(url);
   const data = await response.json();
-  let namesToRemove = filterName.replace(/，/g, ",").split(',');
-  for (let name of namesToRemove) {
-  let nameIndex = data.myMemoList.findIndex(item => (item.creatorName == name));
-    if (nameIndex !== -1) {
-      delete data.myMemoList.splice(nameIndex, 1);
-    }
-  };
+  if(filterName){
+    let namesToRemove = filterName.replace(/，/g, ",").split(',');
+    for (let name of namesToRemove) {
+    let nameIndex = data.myMemoList.findIndex(item => (item.creatorName == name));
+      if (nameIndex !== -1) {
+        delete data.myMemoList.splice(nameIndex, 1);
+      }
+    };
+  }
   return data.myMemoList
 }
 
